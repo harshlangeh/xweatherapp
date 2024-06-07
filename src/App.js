ca9062d75b6e78c106bf352667b71e03bc168084
harshlangeh <harshxhtc@gmail.com>
2024-06-07 08:21:01 +0530

    ui and loading

--- src/App.js
+++ src/App.js
@@ -53,30 +53,30 @@ function App() {
       />
       <button onClick={handleSearch}>Search</button>
 
-      {loading && <p>Loading data…</p>}
-      {error && <p>{error}</p>}
+      {loading ? <p>Loading data…</p> : (
+        weatherData && (
+          <div className="weather-cards">
+  
+            <div className='weather-card'>
+                <h2>Temperature</h2>
+                <p>{weatherData.current.temp_c}°C</p>
+            </div>
+            <div className='weather-card'>
+                <h2>Humidity</h2>
+                <p>{weatherData.current.humidity}%</p>
+            </div>
+            <div className='weather-card'>
+                <h2>Condition</h2>
+                <p>{weatherData.current.condition.text}</p>
+            </div>
+            <div className='weather-card'>
+                <h2>Wind Speed</h2>
+                <p>{weatherData.current.wind_kph} kph</p>
+            </div>
+          </div>
+      ))}
 
-      {weatherData && (
-        <div className="weather-cards">
 
-          <div className='weather-card'>
-              <h2>Temperature</h2>
-              <p>{weatherData.current.temp_c}°C</p>
-          </div>
-          <div className='weather-card'>
-              <h2>Humidity</h2>
-              <p>{weatherData.current.humidity}%</p>
-          </div>
-          <div className='weather-card'>
-              <h2>Condition</h2>
-              <p>{weatherData.current.condition.text}</p>
-          </div>
-          <div className='weather-card'>
-              <h2>Wind Speed</h2>
-              <p>{weatherData.current.wind_kph} kph</p>
-          </div>
-        </div>
-      )}
     </div>
   );
 }
